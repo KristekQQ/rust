25730297152f68465fc4a760fc2db2d34dadc90a
KristekQQ <140430013+KristekQQ@users.noreply.github.com>
2025-07-02 20:28:26 +0200

    Enable camera rotation only on drag

--- src/input/mouse.rs
+++ src/input/mouse.rs
@@ -2,22 +2,61 @@
 
 use std::{cell::RefCell, rc::Rc};
 use wasm_bindgen::{closure::Closure, JsCast};
-use web_sys::Window;
+use web_sys::{Window, HtmlCanvasElement};
 
 use crate::input::camera::CameraController;
 
-pub fn attach<T>(window: &Window, cam: Rc<RefCell<T>>)
+pub fn attach<T>(window: &Window, canvas: &HtmlCanvasElement, cam: Rc<RefCell<T>>)
 where
     T: CameraController + 'static,
 {
-    let cam_mouse = cam.clone();
-    let mouse_move = Closure::wrap(Box::new(move |e: web_sys::MouseEvent| {
-        cam_mouse
-            .borrow_mut()
-            .mouse_move(e.movement_x() as f32, e.movement_y() as f32);
-    }) as Box<dyn FnMut(_)>);
-    window
-        .add_event_listener_with_callback("mousemove", mouse_move.as_ref().unchecked_ref())
-        .unwrap();
-    mouse_move.forget();
+    let dragging = Rc::new(RefCell::new(false));
+
+    // Start dragging only when the left button is pressed on the canvas
+    {
+        let dragging = dragging.clone();
+        let canvas = canvas.clone();
+        let on_down = Closure::wrap(Box::new(move |e: web_sys::MouseEvent| {
+            if e.button() == 0 {
+                if let Some(target) = e.target() {
+                    if target == canvas.clone().dyn_into::<web_sys::EventTarget>().unwrap() {
+                        *dragging.borrow_mut() = true;
+                    }
+                }
+            }
+        }) as Box<dyn FnMut(_)>);
+        canvas
+            .add_event_listener_with_callback("mousedown", on_down.as_ref().unchecked_ref())
+            .unwrap();
+        on_down.forget();
+    }
+
+    // Stop dragging on mouseup anywhere in the window
+    {
+        let dragging = dragging.clone();
+        let on_up = Closure::wrap(Box::new(move |_e: web_sys::MouseEvent| {
+            *dragging.borrow_mut() = false;
+        }) as Box<dyn FnMut(_)>);
+        window
+            .add_event_listener_with_callback("mouseup", on_up.as_ref().unchecked_ref())
+            .unwrap();
+        on_up.forget();
+    }
+
+    // Apply mouse movement only while dragging
+    {
+        let dragging = dragging.clone();
+        let cam_mouse = cam.clone();
+        let mouse_move = Closure::wrap(Box::new(move |e: web_sys::MouseEvent| {
+            if *dragging.borrow() && e.buttons() & 1 == 1 {
+                cam_mouse
+                    .borrow_mut()
+                    .mouse_move(e.movement_x() as f32, e.movement_y() as f32);
+            }
+        }) as Box<dyn FnMut(_)>);
+        window
+            .add_event_listener_with_callback("mousemove", mouse_move.as_ref().unchecked_ref())
+            .unwrap();
+        mouse_move.forget();
+    }
 }
